dbc9bd819284899341ff2bdfd4b50ff676b38075
DESKTOP-0U9M598\M <dksgytjr10@gmail.com>
2025-03-06 17:34:17 +0900

    1. 팀전, 개인전 룸 UI 변경 2. 타이틀 이미지변경

--- Assets/User/Hyoseok/1.Scripts/Scene/TitleController.cs
+++ Assets/User/Hyoseok/1.Scripts/Scene/TitleController.cs
@@ -1,47 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-namespace IdleGame
+
+public class TitleController : MonoBehaviour
 {
-    public class TitleController : MonoBehaviour
+    #region Variables
+    public GameObject menuCanvas;
+    public GameObject optionCanvas;
+
+    public Button newGameButton;
+    public Button tutorialButton;
+    public Button optionsButton;
+    public Button quitGameButton;
+
+
+    #endregion
+
+    private void Start()
     {
-        #region Variables
-        public GameObject menuCanvas;
-        public GameObject optionCanvas;
+        newGameButton.onClick.AddListener(OnNewGame);
+        tutorialButton.onClick.AddListener(OnTutorial);
+        optionsButton.onClick.AddListener(OnOptions);
+        quitGameButton.onClick.AddListener(OnQuitGame);
+    }
+    void OnNewGame()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
 
-        public Button newGameButton;
-        public Button continewButton;
-        public Button optionsButton;
-        public Button quitGameButton;
+    void OnTutorial()
+    {
 
-     
-        #endregion
+    }
 
-        private void Start()
-        {
-            newGameButton.onClick.AddListener(OnNewGame);
-            continewButton.onClick.AddListener(OnContiew);
-            optionsButton.onClick.AddListener(OnOptions);
-            quitGameButton.onClick.AddListener(OnQuitGame);
-        }
-        void OnNewGame()
-        {
-            SceneManager.LoadScene("MainBase");
-        }
+    void OnOptions()
+    {
+        optionCanvas.SetActive(true);
+    }
 
-        void OnContiew()
-        {
+    void OnQuitGame()
+    {
 
-        }
-
-        void OnOptions()
-        { 
-            optionCanvas.SetActive(true);
-        }
-
-        void OnQuitGame()
-        {
-
-        }
     }
 }
